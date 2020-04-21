06f9454bb99d741726fc5347b60f845f708c5741
dado_hp <odadph@gmail.com>
2020-04-21 09:22:05 +0700

    lambda tf fix

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -1,12 +1,12 @@
 resource "aws_lambda_function" "nama_lambda_function" {
   function_name = "nama_lambda_function"
-  description	= "nama_lambda_function"
+  description	  = "nama_lambda_function"
   filename	    = "./templates/lambda.zip" ## zip file script function
-  role		    = "arn:aws:iam::xxx:role/monitoring"
-  handler	    = "lambda.lambda_handler"
-  runtime	    = "python3.7"
+  role		      = "arn:aws:iam::xxx:role/monitoring"
+  handler	      = "lambda.lambda_handler"
+  runtime	      = "python3.7"
   memory_size   = 512
-  timeout	    = 120
+  timeout	      = 120
 
 ## IF Using VPC Custom
   vpc_config {
@@ -16,16 +16,16 @@ resource "aws_lambda_function" "nama_lambda_function" {
 
   environment {
     variables	= {
-      TOKEN	    = "token_bot_telegram"
+      TOKEN	  = "token_bot_telegram"
       USER_ID	= "chat_id_number"
     }
   }
 
   tags = {
-    Name	= "nama_lambda_function"
+    Name	  = "nama_lambda_function"
     Project	= "hello-world"
-    Ticket      = "nomor_tiket"
-    Recipe      = "${var.IAC_REPO}"
+    Ticket  = "nomor_tiket"
+    Recipe  = "${var.IAC_REPO}"
     }
 }
 
